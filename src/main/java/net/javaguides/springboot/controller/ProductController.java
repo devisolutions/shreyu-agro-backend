6e39aad856138c934605e3cba2c51c989c62dd5e
deepakhb <dhbhagtani@gmail.com>
2021-03-16 17:50:07 +0530

    code refactoring

--- src/main/java/net/javaguides/springboot/controller/ProductController.java
+++ src/main/java/net/javaguides/springboot/controller/ProductController.java
@@ -1,19 +1,19 @@
 package net.javaguides.springboot.controller;
 
-import java.util.List;
-
 import net.javaguides.springboot.exception.ResourceNotFoundException;
 import net.javaguides.springboot.model.Product;
 import net.javaguides.springboot.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -41,33 +41,19 @@ public class ProductController {
     public Product getProductByCode(@PathVariable Long code) {
         return productRepository.findById(code).orElse(null);
     }
-/*
-	// update employee rest api
 
-	@PutMapping("/employees/{id}")
-	public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails){
-		Employee employee = productRepository.findById(id)
-				.orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id :" + id));
+    @DeleteMapping("/{code}")
+    public void deleteProductByCode(@PathVariable Long code) {
+        productRepository.deleteById(code);
+    }
 
-		employee.setFirstName(employeeDetails.getFirstName());
-		employee.setLastName(employeeDetails.getLastName());
-		employee.setEmailId(employeeDetails.getEmailId());
-
-		Employee updatedEmployee = productRepository.save(employee);
-		return ResponseEntity.ok(updatedEmployee);
-	}
-
-	// delete employee rest api
-	@DeleteMapping("/employees/{id}")
-	public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable Long id){
-		Employee employee = productRepository.findById(id)
-				.orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id :" + id));
-
-		productRepository.delete(employee);
-		Map<String, Boolean> response = new HashMap<>();
-		response.put("deleted", Boolean.TRUE);
-		return ResponseEntity.ok(response);
-	}
-	*/
+    @PostMapping("/{code}")
+    public Product updateProduct(@PathVariable Long code, @RequestBody Product product) {
+        if (productRepository.existsById(code)) {
+            return productRepository.save(product);
+        } else {
+            throw new ResourceNotFoundException("Cannot update product, as product does not exists");
+        }
+    }
 
 }
